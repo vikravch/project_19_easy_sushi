57908a8463d8622e425dac5070acf1f750d7fd8b
Dmitry Batrakov <beowoolf61@gmail.com>
2024-05-13 18:43:42 +0300

    corrected header

--- src/modules/home_page/presentation/components/header/Header.tsx
+++ src/modules/home_page/presentation/components/header/Header.tsx
@@ -17,25 +17,25 @@ function Header() {
     };
 
 
-    const [isOpen, setIsOpen] = useState(false); // Состояние для отображения/скрытия выпадающего меню
-    const [selectedCity, setSelectedCity] = useState('City'); // Состояние выбранного города
-    const dropdownRef = useRef<HTMLDivElement>(null); // Ссылка на DOM-элемент выпадающего меню
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedCity, setSelectedCity] = useState('City'); 
+    const dropdownRef = useRef<HTMLDivElement>(null); 
 
-    // Обработчик нажатия на кнопку
+   
     const handleButtonClick = () => {
-        setIsOpen(!isOpen); // Инвертируем состояние отображения выпадающего меню
+        setIsOpen(!isOpen);
     };
 
-    // Обработчик выбора города
+   
     const handleCitySelect = (city: string) => {
-        setSelectedCity(city); // Обновляем выбранный город
-        setIsOpen(false); // Закрываем выпадающее меню
+        setSelectedCity(city);
+        setIsOpen(false); 
     };
 
-    // Обработчик клика за пределами выпадающего меню для его закрытия
+   
     const handleClickOutside = (event: MouseEvent) => {
         if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
-            setIsOpen(false); // Закрываем выпадающее меню, если клик был совершен за его пределами
+            setIsOpen(false); 
         }
     }
 
@@ -96,100 +96,6 @@ function Header() {
 }
 
 export default Header;
-
-
-
-
-
-
-
-
-// import React, { useState } from 'react';
-// import Dropdown from 'react-bootstrap/Dropdown';
-// import DropdownButton from 'react-bootstrap/DropdownButton';
-// import searchIcon from '../../../img/searchIcon.png';
-// import { FormControl, InputGroup, Button } from 'react-bootstrap';
-// import '../header/header.scss';
-// import '../../../../../index.css';
-
-// function ContactSection() {
-//     const [selectedCity, setSelectedCity] = useState('City');
-//     const [showSearchBar, setShowSearchBar] = useState(false);
-
-//     const handleCitySelect = (city: string) => {
-//         setSelectedCity(city);
-//     };
-
-//     const handleSearchIconClick = () => {
-//         setShowSearchBar(true);
-//     };
-
-//     const handleSearchBarClose = () => {
-//         setShowSearchBar(false);
-//     };
-
-//     return (     
-//        <header>
-//           <div className='contactInform'>
-//                 <div className='phoneNumber'>
-//                     <span className='telOrd'>Our phones</span>
-//                     <span className='num'>+972 705 188 955</span>
-//                     <span className='num'>+972 705 188 955</span>
-//                     <span className='openHours'>Business hours: 10:00 - 00:00 </span>
-//                 </div>
-//                 <div className='generalSection'>
-//                 <div className='headerLeftSide'>
-//                 <DropdownButton 
-//                     id="dropdown-basic-button" 
-//                     title={selectedCity} 
-//                     variant="underline"
-//                     className="citySelection"
-//                     >
-//                     <Dropdown.Item onClick={() => handleCitySelect('Haifa')}>Haifa</Dropdown.Item>
-//                     <Dropdown.Item onClick={() => handleCitySelect('Tel-aviv')}>Tel-aviv</Dropdown.Item>
-//                     <Dropdown.Item onClick={() => handleCitySelect('Petah Tikva')}>Petah Tikva</Dropdown.Item>
-//                 </DropdownButton>  
-//                </div>
-                
-//                 <div className='headerRightSide'>
-//                <div className='feedBack'>
-//                     <span className='reviews'><a href="./">Reviews</a></span>
-//                </div>
-//                <div className="orders">
-//                     <span className='ord'><a href="./">Delivery and Payment</a></span>
-//                </div>
-//                {showSearchBar ? (
-//                     <form onSubmit={handleSearchBarClose} className="d-flex searchInput">
-//                         <InputGroup >
-//                             <FormControl
-//                                 placeholder="Search..."
-//                                 aria-label="Search"
-//                                 aria-describedby="basic-addon2"
-//                             />
-//                             <InputGroup.Text>
-//                                 <Button 
-//                                    variant="outline" 
-//                                    type="submit"
-//                                    className="searchActiveButton"
-//                                    >Search</Button>
-//                             </InputGroup.Text>
-//                         </InputGroup>
-//                     </form>
-//                 ) : (
-//                     <div className="search" onClick={handleSearchIconClick}>
-//                         <img className='searchImg' src={searchIcon} alt="#"/>
-//                     </div>
-//                 )}
-//                 </div>
-//                 </div>
-//             </div>
-//        </header>
-//     )
-// }
-
-// export default ContactSection;
-
-
 
 
 
